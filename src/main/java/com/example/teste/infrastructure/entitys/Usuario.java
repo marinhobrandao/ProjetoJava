6ba93d0505f80fe006ce663ab92aecd3b73f593e
Gustavo Marinho <marinho.brandao@unesp.br>
2025-09-11 20:20:03 -0300

    Final

---
+++ src/main/java/com/example/teste/infrastructure/entitys/Usuario.java
@@ -0,0 +1,25 @@
+package com.example.teste.infrastructure.entitys;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "usuario")
+@Entity
+
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "nome")
+    private String nome;
+}
